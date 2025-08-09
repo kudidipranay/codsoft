ddebd97af86c53ed5ed44899aeb7a7ea98394259
kudidipranay <pranaykpranay55@gmail.com>
2025-08-09 14:40:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const display = document.getElementById('display');
+
+    function appendValue(value) {
+      if (display.innerText === '0' || display.innerText === 'Error') {
+        display.innerText = value;
+      } else {
+        display.innerText += value;
+      }
+    }
+
+    function clearDisplay() {
+      display.innerText = '0';
+    }
+
+    function deleteLast() {
+      let text = display.innerText;
+      if (text.length <= 1) {
+        display.innerText = '0';
+      } else {
+        display.innerText = text.slice(0, -1);
+      }
+    }
+
+    function calculate() {
+      try {
+        let result = eval(display.innerText);
+        display.innerText = result;
+      } catch (e) {
+        display.innerText = 'Error';
+      }
+    }
